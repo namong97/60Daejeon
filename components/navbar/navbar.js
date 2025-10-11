2930f71cdab4e278faee48cbe9209e616b07b6e2
namong97 <rkaskagur240@gmail.com>
2025-10-11 11:26:52 +0200

    Add shareable tunnel script for virtual preview

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,16 +1,19 @@
 export function navbar() {
+  const pathPrefix = window.location.pathname.includes("/pages/")
+    ? "../components/Images/"
+    : "./components/Images/";
   return ` <div class="navbar-left">
     <div class="nav-checkbtn">
       <span class="material-symbols-outlined"> menu </span>
     </div>
-    <img 
-      src="https://online.kfc.co.in/static/media/kfcLogo.492728c6.svg" 
-      alt="KFC-LOGO"
+    <img
+      src="${pathPrefix}60gye-logo.svg"
+      alt="60계 치킨 로고"
       id="navbar-kfc-logo"
     />
-    <p id="navbar-menu" class="hide-item">Menu</p>
-    <a id="blog" href="https://medium.com/@ankitgadhwe3830/dba6202d9380"><p id="navbar-about" class="hide-item">About</p> </a> 
-    <a id="blog" href="#"><p id="navbar-about" class="hide-item">Deals</p> </a> 
+    <p id="navbar-menu" class="hide-item">메뉴</p>
+    <a id="blog" href="#store"><p id="navbar-about" class="hide-item">매장 소개</p> </a>
+    <a id="blog" href="#benefit"><p id="navbar-deals" class="hide-item">이벤트</p> </a>
   </div>
 
   <div class="navbar-right">
@@ -19,9 +22,9 @@ export function navbar() {
       src="https://images.ctfassets.net/wtodlh47qxpt/6bJdGLRkksNvWP4LI9ZiFF/cb89d6393492fd093e0f99980abfa39e/Account_Icon.svg"
       alt="man-icon"
     />
-    <p id="navbar-account" class="account hide-item">Account</p>
+    <p id="navbar-account" class="account hide-item">마이페이지</p>
 
-    <p id="navbar-price">₹0.00</p>
+    <p id="navbar-price">₩0</p>
     <img id="navbar-cart-bucket"
       src="https://images.ctfassets.net/wtodlh47qxpt/6qtBVFuno7pdwOQ9RIvYm9/d13e9b7242980972cf49beddde2cc295/bucket_cart_icon.svg"
     />
